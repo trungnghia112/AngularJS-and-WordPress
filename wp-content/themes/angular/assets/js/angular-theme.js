cb00d9e9646afe7d68f6d6fb7428d9e7cea8e54f
trungnghia112 <trungnghia112@gmail.com>
2016-01-01 11:42:08 +0700

    [add] Creating an AngularJS Controller

--- wp-content/themes/angular/assets/js/angular-theme.js
+++ wp-content/themes/angular/assets/js/angular-theme.js
@@ -1,1 +1,7 @@
-var wpApp = new angular.module( 'wpAngularTheme', ['ui.router'] );
+var wpApp = new angular.module( 'wpAngularTheme', ['ui.router', 'ngResource'] );
+
+wpApp.factory( 'Posts', function( $resource ) {
+	return $resource( appInfo.api_url + 'posts/:ID', {
+		ID: '@id'
+	})
+})
